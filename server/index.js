9f386f12a53cec2ad4ccb9f0017a563de28ee736
Muhammad Umar <uilyas97@gmail.com>
2019-08-28 13:16:38 +0500

    chawal

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../dist")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is running ${PORT}`));
